c9b7f2b02a43d9cfdcde44fb279601537a5ac389
Olesya Vetoshkina <our_post-office@mail.ru>
2023-08-08 20:30:34 +0300

    HashRouter add commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Calendar } from "./components/Calendar";
 import './App.css';
+import {HashRouter} from 'react-router-dom';
 
 const now = new Date();
 
 export default function App() {
   return (
-    <Calendar date={now} />
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <Calendar date={now} />
+    </HashRouter>    
   );
 }
